480a3eaf77acdefe8da5f2608bb91b0c164209a7
Kai <kat066@ucsd.edu>
2021-05-19 03:35:43 -0700

    instruction queue half completed

--- mips_cpu/mips_core/instruction_queue.sv
+++ mips_cpu/mips_core/instruction_queue.sv
@@ -1,10 +1,9 @@
 `include "mips_core.svh"
 
 interface instruction_Queue_ifc ();
-	logic [1 : 0] Valid_Ready[32];
+	logic valid[32];
+	logic ready[32];
 	decoder_output_ifc instruction[32]();
-	logic [31 : 0] source [32][2];
-	logic [31 : 0] destination [32];
 	logic [31 : 0] active_List_Index[32];
 
 
@@ -18,8 +17,35 @@ module instruction_Queue (
 instruction_Queue_ifc Instr_Queue();
 always_comb begin
 	int count = 0;
-	while (count < 32) begin
+	while (count < 32 && Instr_Queue.valid[count]== 0) begin
+	Instr_Queue.instruction[count].valid=decoded.valid;
+	Instr_Queue.instruction[count].alu_ctl=decoded.alu_ctl;
+	Instr_Queue.instruction[count].is_branch_jump=decoded.is_branch_jump;
+	Instr_Queue.instruction[count].is_jump=decoded.is_jump;
+	Instr_Queue.instruction[count].is_jump_reg=decoded.is_jump_reg;
+	Instr_Queue.instruction[count].branch_target=decoded.branch_target;
 
+	Instr_Queue.instruction[count].is_mem_access=decoded.is_mem_access;
+	Instr_Queue.instruction[count].mem_action=decoded.mem_action;
+
+	Instr_Queue.instruction[count].uses_rs=decoded.uses_rs;
+	Instr_Queue.instruction[count].rs_addr=decoded.rs_addr;
+
+	Instr_Queue.instruction[count].uses_rt=decoded.uses_rt;
+	Instr_Queue.instruction[count].rt_addr=decoded.rt_addr;
+
+	Instr_Queue.instruction[count].uses_immediate=decoded.uses_immediate;
+	Instr_Queue.instruction[count].immediate=decoded.immediate;
+
+	Instr_Queue.instruction[count].uses_rw=decoded.uses_rw;
+	Instr_Queue.instruction[count].rw_addr=decoded.rw_addr;
+	
+	Instr_Queue.instruction[count].is_ll=decoded.is_ll;
+	Instr_Queue.instruction[count].is_sc=decoded.is_sc;
+	Instr_Queue.instruction[count].is_sw=decoded.is_sw;
+	Instr_Queue.valid[count]=1;
+	Instr_Queue.ready[count]=0;
+	Instr_Queue.active_List_Index[count]=0;
 	end
 
 end
