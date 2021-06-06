3dbebb5737904b70a4eb9fc6bfec71d389de2a55
kat066 <kat066@ucsd.edu>
2021-06-05 19:17:20 -0700

    updated to instruction id for instruction queue, fixed some errors

--- mips_cpu/mips_core/instruction_queue.sv
+++ mips_cpu/mips_core/instruction_queue.sv
@@ -29,7 +29,7 @@ interface instruction_Queue_ifc ();
 	logic is_sc[32];
 	logic is_sw[32];
 	logic [4 : 0] active_List_Index[32];
-	logic [`ADDR_WIDTH-1:0] entry_pc [31: 0];
+	logic [`ADDR_WIDTH-1:0] instruction_ID [32];
 
 endinterface
 
@@ -39,11 +39,15 @@ module instruction_Queue (
 	input free_list[64],
 	input active_list_end_flush_signal,		//Current name for a signal that the Active List sends to other modules to tell them
 											//that it is done flushing (and remappping).
+											
+	input [`ADDR_WIDTH-1:0]flushed_instruction_ID;
 	decoder_output_ifc.in decoded,
 	decoder_output_ifc.in register,
-	decoder_output_ifc.out out,
 	hazard_control_ifc.in hazard,
-	pc_ifc.in instruction_pc
+	decoder_output_ifc.out out,
+	output logic [`ADDR_WIDTH-1:0] issued_instruction_ID;
+	
+
 );
 
 instruction_Queue_ifc Instr_Queue();
@@ -52,10 +56,18 @@ logic [4:0] valid_entry_index;				//Index of the Instr_Queue that points to an e
 logic [4:0] ready_and_valid_index;			//Index of the Instr_Queue that points to an element that is ready AND valid!
 
 logic block_queue_from_adding;
+logic [`ADDR_WIDTH-1:0] next_ID;
+
+initial begin
+	for(int i = 0; i < 32;i++) begin
+		Instr_Queue.valid_entry[i] = 1'b0;
+	end
+	next_ID = 32'b0;
+end
 
 always_comb begin
 	for (int i = 0; i < 32; i++) begin
-		ready_and_valid[i] = Instr_Queue.ready[i] & Instr_Queue.valid[i];
+		ready_and_valid[i] = Instr_Queue.ready[i] & Instr_Queue.valid_entry[i];
 	end
 end
 
@@ -93,149 +105,94 @@ always_ff @(posedge clk) begin
 	 *and is "freed" whenever the Active List sends it a signal telling the Instr_Queue that the Active List
 	 *is done flushing!
 	 */
-	if (hazard.flush) block_queue_from_adding <= 1;
-	if (active_list_end_flush_signal) block_queue_from_adding <= 0;
-	
-	if (~block_queue_from_adding) begin
-		Instr_Queue.valid[valid_entry_index]   <= decoded.valid;
-		Instr_Queue.alu_ctl[valid_entry_index] <= decoded.alu_ctl;
-		
-		Instr_Queue.is_branch_jump[valid_entry_index] <= decoded.is_branch_jump;
-		Instr_Queue.is_jump[valid_entry_index] 		  <= decoded.is_jump;
-		Instr_Queue.is_jump_reg[valid_entry_index]    <= decoded.is_jump_reg;
-		Instr_Queue.branch_target[valid_entry_index]  <= decoded.branch_target;
-
-		Instr_Queue.is_mem_access[valid_entry_index] <= decoded.is_mem_access;
-		Instr_Queue.mem_action[valid_entry_index]    <= decoded.mem_action;
-
-		Instr_Queue.uses_rs[valid_entry_index] <= register.uses_rs;
-		Instr_Queue.rs_addr[valid_entry_index] <= register.rs_addr;
-
-		Instr_Queue.uses_rt[valid_entry_index] <= register.uses_rt;
-		Instr_Queue.rt_addr[valid_entry_index] <= register.rt_addr;
-
-		Instr_Queue.uses_immediate[valid_entry_index] <= decoded.uses_immediate;
-		Instr_Queue.immediate[valid_entry_index]      <= decoded.immediate;
-
-		Instr_Queue.uses_rw[valid_entry_index] <= register.uses_rw;
-		Instr_Queue.rw_addr[valid_entry_index] <= register.rw_addr;
-		
-		Instr_Queue.is_ll[valid_entry_index] <= decoded.is_ll;
-		Instr_Queue.is_sc[valid_entry_index] <= decoded.is_sc;
-		Instr_Queue.is_sw[valid_entry_index] <= decoded.is_sw;
-		
-		Instr_Queue.valid_entry[valid_entry_index]		 <= 1;
-		Instr_Queue.ready[valid_entry_index] 	   		 <= (free_list[register.rs_addr] & free_list[register.rt_addr]) ? 'b1 : 'b0;
-		Instr_Queue.active_List_Index[valid_entry_index] <= 0;  //The active_List_Index should be based on where the corresponding 
-																//entry is in the active list!
-		Instr_Queue.entry_pc[valid_entry_index] 		 <= instruction_pc.pc;														
+	if (~rst_n) begin
+		for(int i = 0; i < 32;i++) begin
+			Instr_Queue.valid_entry[i] = 1'b0;
+		end
+		next_ID = 32'b0;
 	end
-
-	/* Now, even though we cannot ADD new instructions into the Instr_Queue while
-	 * a flush is happening, we can still ISSUE them, if they are ready and valid!
-	 */
-	out.valid 	<= Instr_Queue.valid[valid_entry_index];
-	out.alu_ctl <= Instr_Queue.alu_ctl[valid_entry_index];
+	else begin
+		if (hazard.flush) block_queue_from_adding <= 1;
+		if (active_list_end_flush_signal) block_queue_from_adding <= 0;
 	
-	out.is_branch_jump <= Instr_Queue.is_branch_jump[valid_entry_index];
-	out.is_jump		   <= Instr_Queue.is_jump[valid_entry_index];
-	out.is_jump_reg    <= Instr_Queue.is_jump_reg[valid_entry_index];
-	out.branch_target  <= Instr_Queue.branch_target[valid_entry_index];
-
-	out.is_mem_access <= Instr_Queue.is_mem_access[valid_entry_index];
-	out.mem_action 	  <= Instr_Queue.mem_action[valid_entry_index];
-
-	out.uses_rs <= Instr_Queue.uses_rs[valid_entry_index];
-	out.rs_addr <= Instr_Queue.rs_addr[valid_entry_index];
-
-	out.uses_rt <= Instr_Queue.uses_rt[valid_entry_index];
-	out.rt_addr <= Instr_Queue.rt_addr[valid_entry_index];
-
-	out.uses_immediate <= Instr_Queue.uses_immediate[valid_entry_index];
-	out.immediate 	   <= Instr_Queue.immediate[valid_entry_index];
-
-	out.uses_rw <= Instr_Queue.uses_rw[valid_entry_index];
-	out.rw_addr <= Instr_Queue.rw_addr[valid_entry_index];
-	
-	out.is_ll <= Instr_Queue.is_ll[valid_entry_index];
-	out.is_sc <= Instr_Queue.is_sc[valid_entry_index];
-	out.is_sw <= Instr_Queue.is_sw[valid_entry_index];
-	
-	
-	/* Finally, we will have a comparator attached to each entry in the Instr_Queue.
-	 * If the Instr_Queue detects a flush signal, and the PC of the entry is less than
-	 * the current PC, the "valid_entry" bit of the entry will be set to 0, "flushing" it
-	 * from the queue.
-	 */
-	for (int i = 0; i < 32; i++) if ((instruction_pc.pc < Instr_Queue.entry_pc[i]) && hazard.flush) Instr_Queue.valid_entry[i] <= 0;
-end
-
-
-/*
-always_ff @(posedge clk) begin		
-	for(int i=0,j=0;i<32;i++) begin
-		if(~Instr_Queue.valid_entry[i] & ~j) begin
-			Instr_Queue.valid[i]=decoded.valid;
-			Instr_Queue.alu_ctl[i]=decoded.alu_ctl;
-			Instr_Queue.is_branch_jump[i]=decoded.is_branch_jump;
-			Instr_Queue.is_jump[i]=decoded.is_jump;
-			Instr_Queue.is_jump_reg[i]=decoded.is_jump_reg;
-			Instr_Queue.branch_target[i]=decoded.branch_target;
-
-			Instr_Queue.is_mem_access[i]=decoded.is_mem_access;
-			Instr_Queue.mem_action[i]=decoded.mem_action;
-
-			Instr_Queue.uses_rs[i]=register.uses_rs;
-			Instr_Queue.rs_addr[i]=register.rs_addr;
-
-			Instr_Queue.uses_rt[i]=register.uses_rt;
-			Instr_Queue.rt_addr[i]=register.rt_addr;
-
-			Instr_Queue.uses_immediate[i]=decoded.uses_immediate;
-			Instr_Queue.immediate[i]=decoded.immediate;
-
-			Instr_Queue.uses_rw[i]=register.uses_rw;
-			Instr_Queue.rw_addr[i]=register.rw_addr;
+		if (~block_queue_from_adding) begin
+			Instr_Queue.valid[valid_entry_index]   <= decoded.valid;
+			Instr_Queue.alu_ctl[valid_entry_index] <= decoded.alu_ctl;
 			
-			Instr_Queue.is_ll[i]=decoded.is_ll;
-			Instr_Queue.is_sc[i]=decoded.is_sc;
-			Instr_Queue.is_sw[i]=decoded.is_sw;
-			Instr_Queue.valid_entry[i]=1;
-			Instr_Queue.ready[i]=(free_list[register.rs_addr] & free_list[register.rt_addr])?'b1:'b0;
-			Instr_Queue.active_List_Index[i]=0;  //The active_List_Index should be based on where the corresponding 
-												 //entry is in the active list!
-			j=1;
-		end				
-		else if(Instr_Queue.valid_entry[i] & Instr_Queue.ready[i]) begin
-			out.valid=Instr_Queue.valid[i];
-			out.alu_ctl=Instr_Queue.alu_ctl[i];
-			out.is_branch_jump=Instr_Queue.is_branch_jump[i];
-			out.is_jump=Instr_Queue.is_jump[i];
-			out.is_jump_reg=Instr_Queue.is_jump_reg[i];
-			out.branch_target=Instr_Queue.branch_target[i];
+			Instr_Queue.is_branch_jump[valid_entry_index] <= decoded.is_branch_jump;
+			Instr_Queue.is_jump[valid_entry_index] 		  <= decoded.is_jump;
+			Instr_Queue.is_jump_reg[valid_entry_index]    <= decoded.is_jump_reg;
+			Instr_Queue.branch_target[valid_entry_index]  <= decoded.branch_target;
 
-			out.is_mem_access=Instr_Queue.is_mem_access[i];
-			out.mem_action=Instr_Queue.mem_action[i];
+			Instr_Queue.is_mem_access[valid_entry_index] <= decoded.is_mem_access;
+			Instr_Queue.mem_action[valid_entry_index]    <= decoded.mem_action;
 
-			out.uses_rs=Instr_Queue.uses_rs[i];
-			out.rs_addr=Instr_Queue.rs_addr[i];
+			Instr_Queue.uses_rs[valid_entry_index] <= register.uses_rs;
+			Instr_Queue.rs_addr[valid_entry_index] <= register.rs_addr;
 
-			out.uses_rt=Instr_Queue.uses_rt[i];
-			out.rt_addr=Instr_Queue.rt_addr[i];
+			Instr_Queue.uses_rt[valid_entry_index] <= register.uses_rt;
+			Instr_Queue.rt_addr[valid_entry_index] <= register.rt_addr;
 
-			out.uses_immediate=Instr_Queue.uses_immediate[i];
-			out.immediate=Instr_Queue.immediate[i];
+			Instr_Queue.uses_immediate[valid_entry_index] <= decoded.uses_immediate;
+			Instr_Queue.immediate[valid_entry_index]      <= decoded.immediate;
 
-			out.uses_rw=Instr_Queue.uses_rw[i];
-			out.rw_addr=Instr_Queue.rw_addr[i];
+			Instr_Queue.uses_rw[valid_entry_index] <= register.uses_rw;
+			Instr_Queue.rw_addr[valid_entry_index] <= register.rw_addr;
 			
-			out.is_ll=Instr_Queue.is_ll[i];
-			out.is_sc=Instr_Queue.is_sc[i];
-			out.is_sw=Instr_Queue.is_sw[i];
-			break;
+			Instr_Queue.is_ll[valid_entry_index] <= decoded.is_ll;
+			Instr_Queue.is_sc[valid_entry_index] <= decoded.is_sc;
+			Instr_Queue.is_sw[valid_entry_index] <= decoded.is_sw;
+			
+			Instr_Queue.valid_entry[valid_entry_index]		 <= 1;
+			Instr_Queue.ready[valid_entry_index] 	   		 <= (free_list[register.rs_addr] & free_list[register.rt_addr]) ? 'b1 : 'b0;
+			Instr_Queue.active_List_Index[valid_entry_index] <= 0;  //The active_List_Index should be based on where the corresponding 
+																	//entry is in the active list!
+			Instr_Queue.instruction_ID[valid_entry_index]	<= next_ID;
+			next_ID <= next_ID + 1；
+													
+		end
+
+		/* Now, even though we cannot ADD new instructions into the Instr_Queue while
+		 * a flush is happening, we can still ISSUE them, if they are ready and valid!
+		 */
+		out.valid 	<= Instr_Queue.valid[ready_and_valid_index];
+		out.alu_ctl <= Instr_Queue.alu_ctl[ready_and_valid_index];
+		
+		out.is_branch_jump <= Instr_Queue.is_branch_jump[ready_and_valid_index];
+		out.is_jump		   <= Instr_Queue.is_jump[ready_and_valid_index];
+		out.is_jump_reg    <= Instr_Queue.is_jump_reg[ready_and_valid_index];
+		out.branch_target  <= Instr_Queue.branch_target[ready_and_valid_index];
+
+		out.is_mem_access <= Instr_Queue.is_mem_access[ready_and_valid_index];
+		out.mem_action 	  <= Instr_Queue.mem_action[ready_and_valid_index];
+
+		out.uses_rs <= Instr_Queue.uses_rs[ready_and_valid_index];
+		out.rs_addr <= Instr_Queue.rs_addr[ready_and_valid_index];
+
+		out.uses_rt <= Instr_Queue.uses_rt[ready_and_valid_index];
+		out.rt_addr <= Instr_Queue.rt_addr[ready_and_valid_index];
+
+		out.uses_immediate <= Instr_Queue.uses_immediate[ready_and_valid_index];
+		out.immediate 	   <= Instr_Queue.immediate[ready_and_valid_index];
+
+		out.uses_rw <= Instr_Queue.uses_rw[ready_and_valid_index];
+		out.rw_addr <= Instr_Queue.rw_addr[ready_and_valid_index];
+		
+		out.is_ll <= Instr_Queue.is_ll[ready_and_valid_index];
+		out.is_sc <= Instr_Queue.is_sc[ready_and_valid_index];
+		out.is_sw <= Instr_Queue.is_sw[ready_and_valid_index];
+		issued_instruction_ID <= Instr_Queue.instruction_ID[ready_and_valid_index];
+		
+		/* Finally, we will have a comparator attached to each entry in the Instr_Queue.
+		 * If the Instr_Queue detects a flush signal, and the PC of the entry is less than
+		 * the current PC, the "valid_entry" bit of the entry will be set to 0, "flushing" it
+		 * from the queue.
+		 */
+		for (int i = 0; i < 32; i++) if ((flushed_instruction_ID < Instr_Queue.instruction_ID[i]) && hazard.flush) Instr_Queue.valid_entry[i] <= 0;
 		end
 	end
-end
-*/
+	
+
+
 
 endmodule
